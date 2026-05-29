c403e793dfc6c17048cfe64a752e10b53de82815
Maxwell <252437093+cardinal9985@users.noreply.github.com>
2026-05-29 00:16:22 -0400

    feat: add flat, night, voice presets and eq aliases

--- modules/home/maxwell/nostromo/aliases.nix
+++ modules/home/maxwell/nostromo/aliases.nix
@@ -2,8 +2,16 @@
 
 {
   home.shellAliases = {
-    rebuild = "nh os switch ~/dotfiles";
-    clean = "nh clean all";
-    secrets = "sudo SOPS_AGE_KEY_FILE=/persist/secrets/age/keys.txt sops ~/dotfiles/secrets/secrets.yaml";
+    rebuild  = "nh os switch ~/dotfiles";
+    clean    = "nh clean all";
+    secrets  = "sudo SOPS_AGE_KEY_FILE=/persist/secrets/age/keys.txt sops ~/dotfiles/secrets/secrets.yaml";
+
+    # EasyEffects preset switching
+    eq-music  = "easyeffects --load-preset music";
+    eq-gaming = "easyeffects --load-preset gaming";
+    eq-movies = "easyeffects --load-preset movies";
+    eq-flat   = "easyeffects --load-preset flat";
+    eq-night  = "easyeffects --load-preset night";
+    eq-voice  = "easyeffects --load-preset voice";
   };
 }
--- modules/home/maxwell/nostromo/easyeffects.nix
+++ modules/home/maxwell/nostromo/easyeffects.nix
@@ -13,4 +13,7 @@
   xdg.dataFile."easyeffects/output/music.json".source  = ../../../../config/easyeffects/music.json;
   xdg.dataFile."easyeffects/output/gaming.json".source = ../../../../config/easyeffects/gaming.json;
   xdg.dataFile."easyeffects/output/movies.json".source = ../../../../config/easyeffects/movies.json;
+  xdg.dataFile."easyeffects/output/flat.json".source   = ../../../../config/easyeffects/flat.json;
+  xdg.dataFile."easyeffects/output/night.json".source  = ../../../../config/easyeffects/night.json;
+  xdg.dataFile."easyeffects/output/voice.json".source  = ../../../../config/easyeffects/voice.json;
 }

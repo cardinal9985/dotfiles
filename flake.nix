9bca36fecccf2920cc40d4777b05615af233d8df
Maxwell <252437093+cardinal9985@users.noreply.github.com>
2026-06-10 16:16:05 -0400

    fix colmena module imports

--- flake.nix
+++ flake.nix
@@ -76,53 +76,57 @@
   let
     system = "x86_64-linux";
 
+    workstationModules = { host, user }: [
+      disko.nixosModules.disko
+      nix-mineral.nixosModules.nix-mineral
+      impermanence.nixosModules.impermanence
+      home-manager.nixosModules.home-manager
+      nur.modules.nixos.default
+      stylix.nixosModules.stylix
+      {
+        nixpkgs.overlays = [
+          (import ./overlays/deskmat.nix)
+          rocksmith-nix.overlays.default
+        ];
+      }
+      {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.backupFileExtension = "backup-$(date +%Y%m%d%H%M%S)";
+        home-manager.extraSpecialArgs = { inherit host user inputs; };
+        home-manager.users.${user} = import ./home/${user}/${host}.nix;
+        home-manager.sharedModules = [
+          nixcord.homeModules.default
+          spicetify-nix.homeManagerModules.default
+          zen-browser.homeModules.default
+          rocksmith-nix.homeManagerModules.default
+          nix-index-database.homeModules.nix-index
+        ];
+      }
+      ./hosts/${host}/disko.nix
+      ./hosts/${host}/hardware-configuration.nix
+      ./hosts/${host}/configuration.nix
+    ];
+
+    serverModules = { host, user }: [
+      disko.nixosModules.disko
+      impermanence.nixosModules.impermanence
+      sops-nix.nixosModules.sops
+      ./hosts/${host}/disko.nix
+      ./hosts/${host}/hardware-configuration.nix
+      ./hosts/${host}/configuration.nix
+    ];
+
     mkWorkstation = { host, user }: nixpkgs.lib.nixosSystem {
       inherit system;
       specialArgs = { inherit host user inputs; };
-      modules = [
-        disko.nixosModules.disko
-        nix-mineral.nixosModules.nix-mineral
-        impermanence.nixosModules.impermanence
-        home-manager.nixosModules.home-manager
-        nur.modules.nixos.default
-        stylix.nixosModules.stylix
-        {
-          nixpkgs.overlays = [
-            (import ./overlays/deskmat.nix)
-            rocksmith-nix.overlays.default
-          ];
-        }
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.backupFileExtension = "backup-$(date +%Y%m%d%H%M%S)";
-          home-manager.extraSpecialArgs = { inherit host user inputs; };
-          home-manager.users.${user} = import ./home/${user}/${host}.nix;
-          home-manager.sharedModules = [
-            nixcord.homeModules.default
-            spicetify-nix.homeManagerModules.default
-            zen-browser.homeModules.default
-            rocksmith-nix.homeManagerModules.default
-            nix-index-database.homeModules.nix-index
-          ];
-        }
-        ./hosts/${host}/disko.nix
-        ./hosts/${host}/hardware-configuration.nix
-        ./hosts/${host}/configuration.nix
-      ];
+      modules = workstationModules { inherit host user; };
     };
 
     mkServer = { host, user }: nixpkgs.lib.nixosSystem {
       inherit system;
       specialArgs = { inherit host user inputs; };
-      modules = [
-        disko.nixosModules.disko
-        impermanence.nixosModules.impermanence
-        sops-nix.nixosModules.sops
-        ./hosts/${host}/disko.nix
-        ./hosts/${host}/hardware-configuration.nix
-        ./hosts/${host}/configuration.nix
-      ];
+      modules = serverModules { inherit host user; };
     };
   in {
     nixosConfigurations = {
@@ -144,7 +148,7 @@
           sudo = true;
           tags = [ "workstation" ];
         };
-        imports = [ self.nixosConfigurations.nostromo.config ];
+        imports = workstationModules { host = "nostromo"; user = "maxwell"; };
       };
 
       ishimura = { ... }: {
@@ -155,7 +159,7 @@
           sudo = true;
           tags = [ "server" ];
         };
-        imports = [ self.nixosConfigurations.ishimura.config ];
+        imports = serverModules { host = "ishimura"; user = "maxwell"; };
       };
     };
   };

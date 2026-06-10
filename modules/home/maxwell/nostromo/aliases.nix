be4e72188af6d6dce6898ae03381c70c2d67b7bb
Maxwell <252437093+cardinal9985@users.noreply.github.com>
2026-06-10 17:02:29 -0400

    aliases

--- modules/home/maxwell/nostromo/aliases.nix
+++ modules/home/maxwell/nostromo/aliases.nix
@@ -2,15 +2,17 @@
 
 {
   home.shellAliases = {
-    rebuild  = "nh os switch ~/dotfiles";
-    clean    = "nh clean all";
-    update   = "nix flake update ~/dotfiles";
-    secrets  = "sudo SOPS_AGE_KEY_FILE=/persist/secrets/age/keys.txt sops ~/dotfiles/secrets/secrets.yaml";
+    rebuild   = "nh os switch ~/dotfiles";
+    clean     = "nh clean all";
+    update    = "nix flake update ~/dotfiles";
+    secrets   = "sudo SOPS_AGE_KEY_FILE=/persist/secrets/age/keys.txt sops ~/dotfiles/secrets/secrets.yaml";
     eq-music  = "easyeffects --load-preset music";
     eq-gaming = "easyeffects --load-preset gaming";
     eq-movies = "easyeffects --load-preset movies";
     eq-flat   = "easyeffects --load-preset flat";
     eq-night  = "easyeffects --load-preset night";
     eq-voice  = "easyeffects --load-preset voice";
+    deploy    = "colmena apply --on @server";
+    ishimura  = "TERM=xterm-256color ssh -p 36475 maxwell@192.168.254.186";
   };
 }

e8bb56a8451365103f6e53af7e98cbe8a79e375e
Maxwell <252437093+cardinal9985@users.noreply.github.com>
2026-06-11 04:31:26 -0400

    revert: remove btrfs rollback until approach is fixed

--- modules/nixos/ishimura/boot.nix
+++ modules/nixos/ishimura/boot.nix
@@ -2,48 +2,9 @@
 
 {
   boot = {
-    initrd = {
-      supportedFilesystems = [ "btrfs" ];
-      systemd = {
-        enable = true;
-        services.rollback = {
-          description = "Rollback BTRFS root subvolume to blank snapshot";
-          wantedBy = [ "initrd.target" ];
-          after = [ "dev-nvme0n1p2.device" ];
-          requires = [ "dev-nvme0n1p2.device" ];
-          before = [ "sysroot.mount" ];
-          unitConfig.DefaultDependencies = "no";
-          serviceConfig.Type = "oneshot";
-          script = ''
-            set -x
-
-            mkdir -p /btrfs_tmp
-            mount -t btrfs -o subvol=/ /dev/nvme0n1p2 /btrfs_tmp
-
-            if [[ -e /btrfs_tmp/@ ]]; then
-              mkdir -p /btrfs_tmp/old_roots
-              timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/@)" "+%Y-%m-%d_%H-%M-%S")
-              echo "Moving @ to old_roots/$timestamp"
-              mv /btrfs_tmp/@ "/btrfs_tmp/old_roots/$timestamp"
-            fi
-
-            shopt -s nullglob
-            for snapshot in /btrfs_tmp/old_roots/*/; do
-              echo "Deleting old root: $snapshot"
-              btrfs subvolume delete "$snapshot" || echo "WARNING: failed to delete $snapshot"
-            done
-            shopt -u nullglob
-
-            echo "Creating @ from @blank"
-            btrfs subvolume snapshot /btrfs_tmp/@blank /btrfs_tmp/@
-            umount /btrfs_tmp
-          '';
-        };
-      };
-    };
-
     loader = {
       efi.canTouchEfiVariables = true;
+      timeout = 5;
       systemd-boot = {
         enable = true;
         configurationLimit = 20;

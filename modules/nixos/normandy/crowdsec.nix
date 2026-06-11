f9d41beb7a27b3665595c7285fd0ee869c397d04
Maxwell <252437093+cardinal9985@users.noreply.github.com>
2026-06-11 16:02:24 -0400

    fix(crowdsec): remove racy install-collections systemd unit

--- modules/nixos/normandy/crowdsec.nix
+++ modules/nixos/normandy/crowdsec.nix
@@ -1,14 +1,5 @@
-{ config, pkgs, lib, ... }:
+{ ... }:
 
-let
-  hubCollections = [
-    "crowdsecurity/sshd"
-    "crowdsecurity/linux"
-    "crowdsecurity/base-http-scenarios"
-    "crowdsecurity/http-cve"
-    "crowdsecurity/traefik"
-  ];
-in
 {
   services.crowdsec = {
     enable = true;
@@ -24,21 +15,5 @@ in
         labels.type = "traefik";
       }
     ];
-  };
-
-  systemd.services.crowdsec-install-collections = {
-    description = "Install CrowdSec hub collections";
-    wantedBy = [ "multi-user.target" ];
-    after = [ "crowdsec.service" ];
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = true;
-    };
-    script = ''
-      ${pkgs.crowdsec}/bin/cscli hub update
-      ${lib.concatMapStringsSep "\n" (c: ''
-        ${pkgs.crowdsec}/bin/cscli collections install ${c} || true
-      '') hubCollections}
-    '';
   };
 }

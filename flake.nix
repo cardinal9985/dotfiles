005229419ec88b39b104e0561ad88f161a160611
Maxwell <252437093+cardinal9985@users.noreply.github.com>
2026-06-11 06:58:02 -0400

    kill me: 12 hours of hell (and 2 reinstalls)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My System Configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
 
     colmena = {
       url = "github:zhaofengli/colmena";
@@ -22,7 +22,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 

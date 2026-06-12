b6a222aea6e86ca5a677ce396c003c7745ea24e2
Maxwell <252437093+cardinal9985@users.noreply.github.com>
2026-06-11 20:47:49 -0400

    feat(crowdsec): notify ntfy on every ban via http plugin

--- modules/nixos/normandy/crowdsec.nix
+++ modules/nixos/normandy/crowdsec.nix
@@ -23,5 +23,31 @@
         labels.type = "traefik";
       }
     ];
+
+    localConfig.profiles = [
+      {
+        name = "default_ip_remediation";
+        filters = [ ''Alert.Remediation == true && Alert.GetScope() == "Ip"'' ];
+        decisions = [{ type = "ban"; duration = "4h"; }];
+        notifications = [ "ntfy-default" ];
+        on_success = "break";
+      }
+    ];
   };
+
+  environment.etc."crowdsec/notifications/ntfy.yaml".text = ''
+    type: http
+    name: ntfy-default
+    log_level: info
+    format: |
+      {{range . -}}
+      🚫 Banned {{.Source.Value}}{{if .Source.Cn}} ({{.Source.Cn}}){{end}} - {{.Scenario}}
+      {{end -}}
+    url: http://localhost:8080/crowdsec
+    method: POST
+    headers:
+      X-Title: CrowdSec Ban
+      X-Tags: shield,no_entry
+      X-Priority: default
+  '';
 }

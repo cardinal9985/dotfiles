fd6f37e360a2807673f5b6dbd088df126df1f54f
Maxwell <252437093+cardinal9985@users.noreply.github.com>
2026-06-13 22:57:02 -0400

    feat(crowdsec): expose prometheus metrics on tailnet

--- modules/nixos/normandy/crowdsec.nix
+++ modules/nixos/normandy/crowdsec.nix
@@ -2,6 +2,9 @@
 
 {
   networking.firewall.interfaces.podman1.allowedTCPPorts = [ 8081 ];
+  # Expose CrowdSec's Prometheus metrics to the tailnet so ishimura's
+  # Prometheus can scrape it.
+  networking.firewall.interfaces."tailscale0".allowedTCPPorts = [ 6060 ];
 
   environment.etc."crowdsec/parsers/s02-enrich/tailnet-whitelist.yaml".source = pkgs.writeText "tailnet-whitelist.yaml" ''
     name: maxwell/tailnet-whitelist
@@ -18,6 +21,10 @@
     settings = {
       general.api.server.enable = true;
       general.api.server.listen_uri = "0.0.0.0:8081";
+      general.prometheus.enabled = true;
+      general.prometheus.level = "full";
+      general.prometheus.listen_addr = "0.0.0.0";
+      general.prometheus.listen_port = 6060;
       lapi.credentialsFile = "/var/lib/crowdsec/local_api_credentials.yaml";
     };
 

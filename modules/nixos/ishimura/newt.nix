2b6139062b580219ae1743ab796fc904af8d2aea
Maxwell <252437093+cardinal9985@users.noreply.github.com>
2026-06-29 04:47:07 -0400

    fix(ishimura): pin newt to 1.13.0 for pangolin 1.18 wg handshake compat

--- modules/nixos/ishimura/newt.nix
+++ modules/nixos/ishimura/newt.nix
@@ -3,6 +3,20 @@
 let
   siteId   = "y5rmkmov6tg0k2q";
   endpoint = "https://pangolin.ishimura.lol";
+
+  # nixpkgs 26.05 ships fosrl-newt 1.12.4 which uses the old WG handshake.
+  # Pangolin server 1.18.4 broke compat - newt/wg/register times out forever.
+  # Pin to upstream 1.13.0 until nixpkgs bumps.
+  newt = pkgs.fosrl-newt.overrideAttrs (old: rec {
+    version = "1.13.0";
+    src = pkgs.fetchFromGitHub {
+      owner = "fosrl";
+      repo = "newt";
+      rev = version;
+      hash = "sha256-Kt7YCxHQEv1DeASPJtjAwzmAiWBrkf+XNs7aJEZvb+M=";
+    };
+    vendorHash = "sha256-QJ70q53k4EvLpiMY+Nm70QqaZk14V0Q1CrwWVSowdUU=";
+  });
 in
 {
   systemd.services.newt = {
@@ -33,7 +47,7 @@ in
     '';
 
     script = ''
-      exec ${pkgs.fosrl-newt}/bin/newt --config-file /run/newt/config.json
+      exec ${newt}/bin/newt --config-file /run/newt/config.json
     '';
   };
 }
